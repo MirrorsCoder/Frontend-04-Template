8e443a276113f213815f025ee964fa50b3e78657
丁靖祥 <2671466204@qq.com>
2020-10-21 10:13:25 +0800

    Week_07 finished

---
+++ Week_07/server.js
@@ -0,0 +1,44 @@
+const http = require('http')
+
+http
+  .createServer((req, res) => {
+    let body = []
+    req
+      .on('error', (err) => {
+        console.error(err)
+      })
+      .on('data', (chunk) => {
+        body.push(chunk.toString())
+      })
+      .on('end', () => {
+        // body = Buffer.concat(body).toString()
+        console.log('body', body)
+        res.writeHead(200, {
+          'Content-Type': 'text/html',
+        })
+        res.end(`
+          <html maaa=a >
+          <head>
+            <style>
+              body div #myid{
+                width:100px
+                background-color:#ff5000
+              }
+              body div img{
+                width:30px
+                background-color:#ff1111
+              }
+            </style>
+          </head>
+          <body>
+            <div>
+              <img id="myid" />
+              <img />
+            </div>
+          </body>
+        `)
+      })
+  })
+  .listen(8088)
+
+console.log('server started')
